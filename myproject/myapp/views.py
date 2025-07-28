2a2a967fb56e66c1f7ee2b47f250710832dbcc40
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-07-28 10:19:55 +0530

    #4th day

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-def mrectHome(request):
+def contact(request):
     return HttpResponse("this is myapp")
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
  path('admin/', admin.site.urls),
- path()
-
+ path('',views.home),
+ path('about/',views.about),
+ path('contact/', include('myapp.urls')),
 ]
