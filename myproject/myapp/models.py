9b0ae810670a18744f6cba2269bdeec72fb8d537
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-07-29 12:33:57 +0530

    #5th day

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    name=models.CharField(max_length=100)
+    rollno=models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}-{self.rollno}"
+
